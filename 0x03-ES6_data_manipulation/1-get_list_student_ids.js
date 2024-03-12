652e562f5032efe6e3695888812959902e1c40d5
Daniel Oladele <danieloladele7@gmail.com>
2024-03-12 15:51:27 +0200

    Task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(arr) {
   let newArr = [];
-  if (array instanceof Array) {
-    newArr = array.map((item) => item.id);
+  if (arr instanceof Array) {
+    newArr = arr.map((item) => item.id);
   }
 
   return newArr;
